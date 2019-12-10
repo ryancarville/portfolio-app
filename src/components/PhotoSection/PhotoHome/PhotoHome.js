505400e9a4e8c16376134c16484095503dce3796
Ryan Carville <ryancarville@gmail.com>
2019-12-10 13:14:37 +0100

    photo home

--- src/components/PhotoSection/PhotoHome/PhotoHome.js
+++ src/components/PhotoSection/PhotoHome/PhotoHome.js
@@ -4,20 +4,22 @@ import HomeGallery from "../Galleries/HomeGallery";
 
 export default function PhotoHome(props) {
   return (
-    <section id="photo-home-wrapper">
-      <HomeGallery />
-      <aside id="photo-home-content">
-        <header id="photo-home-header">
-          <h1>Content Creation</h1>
-        </header>
-        <article>
-          Image creation was my first true love. With over 10 years of
-          experience in the commercial advertising and e-commerce industries, I
-          obtain a vast knowledge on how to bring your brand imaging to life on
-          the web.
-        </article>
-      </aside>
-      <div id="photo-home-text-background"></div>
-    </section>
+    <>
+      <section className="section-wrapper" id="photo-home-wrapper">
+        <HomeGallery />
+        <aside id="photo-home-content">
+          <header id="photo-home-header">
+            <h1>Content Creation</h1>
+          </header>
+          <article>
+            Image creation was my first true love. With over 10 years of
+            experience in the commercial advertising and e-commerce industries,
+            I obtain a vast knowledge on how to bring your brand imaging to life
+            on the web.
+          </article>
+          <div id="photo-home-text-background"></div>
+        </aside>
+      </section>
+    </>
   );
 }
