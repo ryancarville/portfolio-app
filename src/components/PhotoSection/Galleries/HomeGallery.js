6ead103244fa1c6b3ad937925522a0f94bc19489
Ryan Carville <ryancarville@gmail.com>
2019-12-16 13:40:03 +0100

    fixed home slideshow

--- src/components/PhotoSection/Galleries/HomeGallery.js
+++ src/components/PhotoSection/Galleries/HomeGallery.js
@@ -1,12 +1,11 @@
 import React, { Component } from "react";
 import "./HomeGallery.css";
 
-
 export default class HomeGallery extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      imgUrl: "https://beardystudios.com/dev_site_images/home-gallery/01.jpg",
+      imgUrl: "https://beardystudios.com/dev_site_images/home-gallery/01.png",
       alt: "home-galley-1"
     };
   }
@@ -14,24 +13,39 @@ export default class HomeGallery extends Component {
     let i = 2;
     setInterval(() => {
       this.setState({
-        imgUrl: `https://beardystudios.com/dev_site_images/home-gallery/0${i}.jpg`,
+        imgUrl: `https://beardystudios.com/dev_site_images/home-gallery/0${i}.png`,
         alt: `home-galley-${i}`
       });
       i++;
       if (i === 4) {
         i = 1;
       }
-    }, 9000);
+    }, 6000);
   };
   componentDidMount() {
     window.scroll(0, 0);
     this.rotateImages();
+    this.startFade();
   }
+  swapImage = () => {
+    console.log("test");
+    const image = document.querySelector("#home-gallery-img");
+    image.classList.add("fade-out");
+  };
+  startFade = () => {
+    const images = document.querySelector("#home-gallery-img");
+    console.log(images);
+    images.addEventListener("transitionend", this.swapImage);
+  };
 
   render() {
     return (
       <section id="home-gallery">
-        <img src={this.state.imgUrl} alt={this.state.alt} />
+        <img
+          id="home-gallery-img"
+          src={this.state.imgUrl}
+          alt={this.state.alt}
+        />
       </section>
     );
   }
